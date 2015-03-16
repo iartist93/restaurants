3b86f00f399e5615023c044cc666e9fb59774e98
Ahmad Ayman <iartist93@Ahmads-MacBook-Pro.local>
2015-03-16 22:22:30 +0200

    Initial Commit

---
+++ AppCodeSwiftBeginning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppCodeSwiftBeginning
+//
+//  Created by Ahmad Ayman on 3/16/15.
+//  Copyright (c) 2015 Ahmad Ayman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
